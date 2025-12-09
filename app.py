0af598ba8a9ed8ccae047bff5186cd0adfa89903
jareddarkweb <motocross7879>
2025-12-09 10:47:16 -0700

    update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, world!"
+    return "POC By Jared-Calgary-Cyber-Security@wearehackerone!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
